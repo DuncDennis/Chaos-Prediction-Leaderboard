74c63b3952fc48b678ee942cb1d32778b7dfac6d
DuncDennis <90915296+DuncDennis@users.noreply.github.com>
2023-02-20 21:07:32 +0100

    First very simple Leaderboard.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,18 +3,38 @@ import streamlit as st
 from google.cloud import firestore
 from google.oauth2 import service_account
 import json
+import pandas as pd
+import datetime
 
+LEADERBOARD_STR = "results"
 
+# get db connection:
 key_dict = json.loads(st.secrets["textkey"])
 creds = service_account.Credentials.from_service_account_info(key_dict)
 db = firestore.Client(credentials=creds, project="chaos-prediction-leaderboard")
 
-# Create a reference to the Google post.
-doc_ref = db.collection("results").document("testuser")
+# Add entry:
+with st.sidebar:
+    st.header("Add entry:")
+    name = st.text_input("Name")
+    valid_time = st.number_input("Valid time")
+    time = datetime.datetime.now()
+    ID = name + " " + str(time)
+    data_to_add = {"name": name,
+                   "date": time,
+                   "valid time": valid_time}
 
-# Then get the data at that reference.
-doc = doc_ref.get()
+    if st.button("submit"):
+        db.collection(LEADERBOARD_STR).document(ID).set(data_to_add)
 
-# Let's see what we got!
-st.write("The id is: ", doc.id)
-st.write("The contents are: ", doc.to_dict())
+# show leaderboard:
+results_ref = db.collection(LEADERBOARD_STR)
+docs = results_ref.stream()
+items = list(map(lambda x: x.to_dict(), docs))
+df = pd.DataFrame(items)
+df = df[["name", "date", "valid time"]]
+st.header("Leaderboard")
+st.dataframe(df)
+
+
+
