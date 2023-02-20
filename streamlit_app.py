9da0c16dcae45f89261908b6146463bee970d5f4
DuncDennis <90915296+DuncDennis@users.noreply.github.com>
2023-02-20 21:58:07 +0100

    Caching DB connection.

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,26 +6,42 @@ import json
 import pandas as pd
 import datetime
 
+@st.cache_resource
+def connect_to_db():
+    """Connect to firestore database and return database object."""
+    print("db connection made")
+    key_dict = json.loads(st.secrets["textkey"])
+    creds = service_account.Credentials.from_service_account_info(key_dict)
+    db = firestore.Client(credentials=creds, project="chaos-prediction-leaderboard")
+    return db
+
+
 LEADERBOARD_STR = "results"
 
 # get db connection:
-key_dict = json.loads(st.secrets["textkey"])
-creds = service_account.Credentials.from_service_account_info(key_dict)
-db = firestore.Client(credentials=creds, project="chaos-prediction-leaderboard")
+db = connect_to_db()
 
 # Add entry:
 with st.sidebar:
     st.header("Add entry:")
-    name = st.text_input("Name")
-    valid_time = st.number_input("Valid time")
-    time = datetime.datetime.now()
-    ID = name + " " + str(time)
-    data_to_add = {"name": name,
-                   "date": time,
-                   "valid time": valid_time}
+    with st.form(key="entry"):
+        name = st.text_input("Name")
+        valid_time = st.number_input("Valid time")
+        time = datetime.datetime.now()
 
-    if st.button("submit"):
-        db.collection(LEADERBOARD_STR).document(ID).set(data_to_add)
+        ID = name + " " + str(time)
+        data_to_add = {"name": name,
+                       "date": time,
+                       "valid time": valid_time}
+        submitted = st.form_submit_button("Submit")
+        if submitted:
+            # Check arguments
+            # ...
+
+            # submit to database
+            db.collection(LEADERBOARD_STR).document(ID).set(data_to_add)
+
+
 
 # show leaderboard:
 results_ref = db.collection(LEADERBOARD_STR)
@@ -33,6 +49,9 @@ docs = results_ref.stream()
 items = list(map(lambda x: x.to_dict(), docs))
 df = pd.DataFrame(items)
 df = df[["name", "date", "valid time"]]
+# df["date"] = df["date"].dt.strftime("%Y-%m-%d %H:%M:%S")
+df["date"] = df["date"].dt.strftime("%d-%m-%Y %H:%M")
+df.sort_values(by="valid time", inplace=True)
 st.header("Leaderboard")
 st.dataframe(df)
 
