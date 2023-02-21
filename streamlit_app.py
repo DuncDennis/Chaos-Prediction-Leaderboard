581ceab1cc77646b52fd6df99ac83c2baab28fb0
DuncDennis <90915296+DuncDennis@users.noreply.github.com>
2023-02-21 10:35:05 +0100

    Caching and reload button.

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,49 +11,75 @@ def connect_to_db():
     """Connect to firestore database and return database object."""
     print("db connection made")
     key_dict = json.loads(st.secrets["textkey"])
-    creds = service_account.Credentials.from_service_account_info(key_dict)
-    db = firestore.Client(credentials=creds, project="chaos-prediction-leaderboard")
+    creds = service_account.Credentials.from_service_account_info(key_dict)  # no read/write
+    db = firestore.Client(credentials=creds, project="chaos-prediction-leaderboard")  #
     return db
 
+def clear_cache_data():
+    """Clear st.cache_data to reload leaderboard."""
+    print("cache cleared")
+    st.cache_data.clear()
+
+@st.cache_data
+def read_leaderboard():
+    """Read leaderboard from database."""
+    print("db read out made")
+    results_ref = db.collection(LEADERBOARD_STR)
+    docs = results_ref.stream()
+    items = list(map(lambda x: x.to_dict(), docs))
+    df = pd.DataFrame(items)
+    df = df[["name", "date", "valid time"]]
+    df["date"] = df["date"].dt.strftime("%d-%m-%Y %H:%M")
+    df.sort_values(by="valid time", inplace=True)
+    return df
 
 LEADERBOARD_STR = "results"
 
-# get db connection:
-db = connect_to_db()
+if __name__ == "__main__":
+    st.set_page_config("Chaos Prediction Leaderboard", page_icon="🎯")
 
-# Add entry:
-with st.sidebar:
-    st.header("Add entry:")
-    with st.form(key="entry"):
-        name = st.text_input("Name")
-        valid_time = st.number_input("Valid time")
-        time = datetime.datetime.now()
+    # get db connection:
+    db = connect_to_db()
 
-        ID = name + " " + str(time)
-        data_to_add = {"name": name,
-                       "date": time,
-                       "valid time": valid_time}
-        submitted = st.form_submit_button("Submit")
-        if submitted:
-            # Check arguments
-            # ...
+    # Add entry:
+    with st.sidebar:
+        st.header("Add entry:")
+        with st.form(key="entry"):
+            name = st.text_input("Name")
+            valid_time = st.number_input("Valid time")
+            time = datetime.datetime.now()
 
-            # submit to database
-            db.collection(LEADERBOARD_STR).document(ID).set(data_to_add)
+            ID = name + " " + str(time)
+            data_to_add = {"name": name,
+                           "date": time,
+                           "valid time": valid_time}
+            submitted = st.form_submit_button("Submit")
+            if submitted:
+                # Check arguments
+                # ...
+
+                # submit to database
+                db.collection(LEADERBOARD_STR).document(ID).set(data_to_add)  # WRITE DATA
+
+                # Clear cache to reload Leaderboard:
+                clear_cache_data()
 
 
+    # show leaderboard:
+    # results_ref = db.collection(LEADERBOARD_STR)  # Creates "1x write"
+    # docs = results_ref.stream()  # Does not read data yet
+    # items = list(map(lambda x: x.to_dict(), docs))
+    # df = pd.DataFrame(items)
+    # df = df[["name", "date", "valid time"]]
+    # # df["date"] = df["date"].dt.strftime("%Y-%m-%d %H:%M:%S")
+    # df["date"] = df["date"].dt.strftime("%d-%m-%Y %H:%M")
+    # df.sort_values(by="valid time", inplace=True)
+    df = read_leaderboard()
 
-# show leaderboard:
-results_ref = db.collection(LEADERBOARD_STR)
-docs = results_ref.stream()
-items = list(map(lambda x: x.to_dict(), docs))
-df = pd.DataFrame(items)
-df = df[["name", "date", "valid time"]]
-# df["date"] = df["date"].dt.strftime("%Y-%m-%d %H:%M:%S")
-df["date"] = df["date"].dt.strftime("%d-%m-%Y %H:%M")
-df.sort_values(by="valid time", inplace=True)
-st.header("Leaderboard")
-st.dataframe(df)
+    st.header("Leaderboard")
+    if st.button("🔄"):
+        clear_cache_data()
+    st.dataframe(df)
 
 
 
